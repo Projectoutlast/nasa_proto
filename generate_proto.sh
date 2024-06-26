f2cf2afbb6bf5144277fa38b70e1e1fcb36052db
Viktor Salimov <viktorsalimov544@gmail.com>
2024-06-26 11:35:50 +0300

    - set-up pre-commit;

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-PROTO_DIR=./proto
-OUT_DIR=./gen
+PROTO_DIR="proto"
+OUT_DIR="gen"
 
-mkdir -p $OUT_DIR
+mkdir -p "$OUT_DIR"
 
-protoc -I proto $PROTO_DIR/*.proto --go_out=$OUT_DIR --go_opt=paths=source_relative --go-grpc_out=$OUT_DIR --go-grpc_opt=paths=source_relative
+protoc -I $PROTO_DIR $PROTO_DIR/*.proto --go_out=./$OUT_DIR --go_opt=paths=source_relative --go-grpc_out=./$OUT_DIR --go-grpc_opt=paths=source_relative
+
+echo "Protobuf code generated successfully."
