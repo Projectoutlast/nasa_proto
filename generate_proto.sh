3c66120358839fb89cef84ea16f05df67d39d260
Viktor Salimov <viktorsalimov544@gmail.com>
2024-06-26 11:37:17 +0300

    - pre-commit;

--- generate_proto.sh
+++ generate_proto.sh
@@ -4,7 +4,6 @@ PROTO_DIR="proto"
 OUT_DIR="gen"
 
 mkdir -p "$OUT_DIR"
-
-protoc -I $PROTO_DIR $PROTO_DIR/*.proto --go_out=./$OUT_DIR --go_opt=paths=source_relative --go-grpc_out=./$OUT_DIR --go-grpc_opt=paths=source_relative
+protoc -I $PROTO_DIR $PROTO_DIR/nasa.proto --go_out=./$OUT_DIR/ --go_opt=paths=source_relative --go-grpc_out=./$OUT_DIR/ --go-grpc_opt=paths=source_relative
 
 echo "Protobuf code generated successfully."
