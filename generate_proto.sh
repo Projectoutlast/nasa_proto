2fb2c7b43a54b07a9cc7ae60a72ee26bfb231870
Viktor Salimov <viktorsalimov544@gmail.com>
2024-06-26 11:29:21 +0300

    - set-up pre-commit;

--- generate_proto.sh
+++ generate_proto.sh
@@ -5,5 +5,4 @@ OUT_DIR=./gen
 
 mkdir -p $OUT_DIR
 
-do protoc -I proto proto/*.proto --go_out=./gen --go_opt=paths=source_relative --go-grpc_out=./gen --go-grpc_opt=paths=source_relative
-done
+protoc -I proto proto/*.proto --go_out=./gen --go_opt=paths=source_relative --go-grpc_out=./gen --go-grpc_opt=paths=source_relative
